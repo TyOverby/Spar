0821aef734fd6fcbe126afb0871a5f2ec7bb0982
TyOverby <ty@pre-alpha.com>
2013-04-03 14:31:42 -0700

    improvements on interpreter

--- src/main/scala/com/tyoverby/macrolisp/interp/Interpreter.scala
+++ src/main/scala/com/tyoverby/macrolisp/interp/Interpreter.scala
@@ -8,31 +8,36 @@ import com.tyoverby.macrolisp.parsers.lisp.{Token => LispToken}
 object Interpreter extends App {
   // Compiler
   val rulesFiles = List("math", "flow", "list", "object", "set", "last").map("./src/test/resources/standard/" + _ + ".rules")
-  val rules = PublicProducer.parseAllRules(rulesFiles: _*)
+  var rules = PublicProducer.parseAllRules(rulesFiles: _*)
 
   val context = Context.enter()
   //  val shell = new Shell() {}
-  val scope = context.initStandardObjects()
-
-  val input = new BufferedReader(new InputStreamReader(System.in))
+  val scope = context.initStandardObjects(Globals)
 
   var line = 0
 
   while (true) {
-    val sb = new StringBuilder
-    var seen = false
+    val i = InterpreterHelper.retrieveStatement()
 
-    do {
-      if (!seen) print("> ") else print("... ")
-      sb.append(input.readLine())
-    } while (!Helper.sameParens(sb.toList))
+    val input = if (i.startsWith("#reload")){
+      rules = PublicProducer.parseAllRules(rulesFiles: _*)
+      println("<< rules reloaded >>")
+      InterpreterHelper.retrieveStatement()
+    } else {
+      i
+    }
 
     try {
+      val actualInput = if (input.startsWith("#generate")) input.replace("#generate", "") else input
 
-      val program: (String, List[LispToken]) = PublicProducer.parseStringSource(sb.toString())
+
+      val program: (String, List[LispToken]) = PublicProducer.parseStringSource(actualInput)
       val compiled = PublicProducer.compile(program, rules)
       line += 1
-      val result = context.evaluateString(scope, compiled._2, "<terminal>", line, null)
+      val result =
+        if (input.startsWith("#generate")) compiled._2
+        else context.evaluateString(scope, compiled._2, "<terminal>", line, null)
+
 
       println(s"$result")
     }
@@ -48,8 +53,11 @@ object Interpreter extends App {
   }
 }
 
-object Globals {
+object Globals extends ScriptableObject {
   val names = Array("print")
+
+//  defineFunctionProperties(names, this.getClass, ScriptableObject.DONTENUM)
+
 
   def print(cx: Context, thisObj: Scriptable, args: Array[Object], funObj: Function) {
     for (i <- 0 until args.length) {
@@ -63,22 +71,6 @@ object Globals {
     }
     System.out.println()
   }
-}
 
-object Helper {
-  def sameParens(str: List[Char]): Boolean = {
-    def helper(str: List[Char], parenCount: Int, bracketCount: Int, curleyCount: Int): Boolean = {
-      str match {
-        case Nil => parenCount == 0 && bracketCount == 0 && curleyCount == 0
-        case '(' :: xs => helper(xs, parenCount + 1, bracketCount, curleyCount)
-        case ')' :: xs => helper(xs, parenCount - 1, bracketCount, curleyCount)
-        case '[' :: xs => helper(xs, parenCount, bracketCount + 1, curleyCount)
-        case ']' :: xs => helper(xs, parenCount, bracketCount - 1, curleyCount)
-        case '{' :: xs => helper(xs, parenCount, bracketCount, curleyCount + 1)
-        case '}' :: xs => helper(xs, parenCount, bracketCount, curleyCount - 1)
-        case _ :: xs => helper(xs, parenCount, bracketCount, curleyCount)
-      }
-    }
-    helper(str, 0, 0, 0)
-  }
+  def getClassName = "Globals"
 }
